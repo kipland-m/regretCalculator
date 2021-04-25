fd21e2ca4bea1ecf8a58911d4e323d4a96a4a7b3
Kipland Melton <kipland@Kiplands-MacBook-Pro.local>
2021-04-25 14:17:27 -0500

    Added text boxes, and labels

--- gui.py
+++ gui.py
@@ -4,19 +4,54 @@
 # Kipland Melton
 
 import sys
-from PyQt5.QtWidgets import QApplication, QLabel
+import PyQt5.QtWidgets as qtw
 
-class Window():
+class MainWindow(qtw.QWidget):
 
+	# The init method will declare all window logic (title, cont)
     def __init__(self):
         super().__init__()
-        app = QApplication([])
-        label = QLabel("test oop")
-        label.show()
-        app.exec_()
+        self.setWindowTitle("RegretCalculator")
+        self.setLayout(qtw.QVBoxLayout())
+        self.keypad()
+
+
+        self.show()
+
+    # This is going to holds all other elements
+    def keypad(self):
+    	container = qtw.QWidget()
+    	container.setLayout(qtw.QGridLayout())
+
+    	# Button Text labels
+    	first_val_label = qtw.QLabel("First Value")
+    	second_val_label = qtw.QLabel("Second Value")
+    	shares_val_label = qtw.QLabel("# of Shares")
+
+    	# Buttons
+    	first_val = qtw.QLineEdit()
+    	second_val = qtw.QLineEdit()
+    	shares_val = qtw.QLineEdit()
+    	btn_result = qtw.QPushButton("Calculate")
+
+    	# Now you must add the buttons to the layout
+    	# With each addWidget 
+
+    	container.layout().addWidget(first_val_label,0,0,1,1)
+    	container.layout().addWidget(second_val_label,0,1,1,1)
+    	container.layout().addWidget(shares_val_label,0,2,1,1)
+
+    	container.layout().addWidget(first_val,1,0,1,1)
+    	container.layout().addWidget(second_val,1,1,1,1)
+    	container.layout().addWidget(shares_val,1,2,1,1)
+
+    	container.layout().addWidget(btn_result,2,1,1,1)
+
+    	self.layout().addWidget(container)
+
 
 
 if __name__ == "__main__":
-    Window()
-
-
+    app = qtw.QApplication([])
+    mw = MainWindow()
+    app.exec_()
