f0fcf714eadbc4a6e1227fcaaa4bd199167c8e3d
Kipland Melton <kipland@kiplands-mbp.home>
2021-04-28 17:41:15 -0500

    Working on adding logic for text boxes

--- gui.py
+++ gui.py
@@ -14,8 +14,6 @@ class MainWindow(qtw.QWidget):
         self.setWindowTitle("RegretCalculator")
         self.setLayout(qtw.QVBoxLayout())
         self.keypad()
-
-
         self.show()
 
     # This is going to holds all other elements
@@ -28,12 +26,13 @@ class MainWindow(qtw.QWidget):
     	second_val_label = qtw.QLabel("Second Value")
     	shares_val_label = qtw.QLabel("# of Shares")
 
-    	# Buttons
-    	first_val = qtw.QLineEdit()
-    	second_val = qtw.QLineEdit()
-    	shares_val = qtw.QLineEdit()
-    	btn_result = qtw.QPushButton("Calculate")
 
+    	# Buttons
+    	self.first_val = qtw.QLineEdit(self)
+    	self.second_val = qtw.QLineEdit(self)
+    	self.shares_val = qtw.QLineEdit(self)
+    	self.btn_result = qtw.QPushButton("Calculate",self)
+		
     	# Now you must add the buttons to the layout
     	# With each addWidget 
 
@@ -41,13 +40,20 @@ class MainWindow(qtw.QWidget):
     	container.layout().addWidget(second_val_label,0,1,1,1)
     	container.layout().addWidget(shares_val_label,0,2,1,1)
 
-    	container.layout().addWidget(first_val,1,0,1,1)
-    	container.layout().addWidget(second_val,1,1,1,1)
-    	container.layout().addWidget(shares_val,1,2,1,1)
+    	container.layout().addWidget(self.first_val,1,0,1,1)
+    	container.layout().addWidget(self.second_val,1,1,1,1)
+    	container.layout().addWidget(self.shares_val,1,2,1,1)
 
-    	container.layout().addWidget(btn_result,2,1,1,1)
+    	container.layout().addWidget(self.btn_result,2,1,1,1)
+
+    	self.btn_result.clicked.connect(self.on_click)
 
     	self.layout().addWidget(container)
+
+    def val_input(self, text):
+    	textBoxValue = self.first_val.text()
+    	QMessageBox.question(self,'message','you typed'+textBoxValue,QMessageBox.ok,QMessageBox.ok)
+    	self.first_val.setText("")
 
 
 
